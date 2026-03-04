ac8ab1a40d5633eedf52b3fdc40ac3634f50747b
Swapnil Tripathi <swapniltripathi484@gmail.com>
2026-03-04 18:58:36 +0530

    Time: 2 ms (68.81%), Space: 42.8 MB (80.97%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+
+        // If i reached end of s, all characters were matched
+        return i == s.length();
+    }
+}
