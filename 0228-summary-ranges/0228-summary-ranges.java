b5c6d85f5d82d6f7b3992f4006fe85dadaad4124
Swapnil Tripathi <swapniltripathi484@gmail.com>
2026-03-17 09:29:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int start = nums[i]
+
+            // move while consecutive
+            while (i < nums.length - 1 && nums[i] + 1 == nums[i + 1]) {
+                i++;
+            }
+
+            // check if range or single
+            if (start != nums[i]) {
+                result.add(start + "->" + nums[i]);
+            } else {
+                result.add(start + "");
+            }
+        }
+
+        return result;
+    }
+}
