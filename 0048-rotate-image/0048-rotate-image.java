88489e63494b0c99f5482ca5c075efafe211898e
Swapnil Tripathi <swapniltripathi484@gmail.com>
2026-05-13 21:55:53 +0530

    Time: 0 ms (100%), Space: 43.5 MB (90.92%) - LeetHub - Time: O(n^2), Space: O(1)

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void rotate(int[][] arr) {
+        for(int i = 0 ; i<arr.length ; i++){
+            for(int j = i; j<arr.length ; j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+
+        for(int i = 0 ; i<arr.length ; i++){
+            int left = 0;
+            int right = arr.length-1;
+            while(left<right){
+                int temp = arr[i][left];
+                arr[i][left] = arr[i][right];
+                arr[i][right] = temp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
