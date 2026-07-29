340e09b6d6e88b77c05395df94c2f026b9258ff1
Swapnil Tripathi <swapniltripathi484@gmail.com>
2026-07-29 09:32:33 +0530

    Time: 3 ms (58.35%), Space: 47.4 MB (11.01%) - LeetHub - Time: O(n), Space: O(n)

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,9 +1,8 @@
-import java.util.*;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer,Integer> map = new HashMap<>();
-        for(int i = 0 ; i<nums.length ; i++){
-            int find = target - nums[i];
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i= 0 ; i<nums.length ; i++){
+            int find = target-nums[i];
             if(map.containsKey(find)){
                 return new int[]{i,map.get(find)};
             }
@@ -12,3 +11,7 @@ class Solution {
         return null;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
