38dce515f5b5e67af8e901ef1e0d53905bb4e0fc
Swapnil Tripathi <swapniltripathi484@gmail.com>
2026-08-09 11:14:55 +0530

    Added solution - Time: O(V + E), Space: O(V + E)

--- 700219-undirected-graph-cycle-GFG/700219-undirected-graph-cycle.java
+++ 700219-undirected-graph-cycle-GFG/700219-undirected-graph-cycle.java
@@ -16,22 +16,47 @@ class Solution {
         boolean[] visited = new boolean[V];
         
         for(int i = 0; i<V ;i++){
-            if(!visited[i] && isCycleDfs(adj,i,visited,-1)){
+            if(!visited[i] && isCycleBfs(adj,i,visited)){
                 return true;
             }
         }
         return false;
     }
     
-    public boolean isCycleDfs(ArrayList<ArrayList<Integer>> adj , int u, boolean[] visited , int parent){
+    
+    public boolean isCycleBfs(ArrayList<ArrayList<Integer>> adj , int u, boolean[] visited){
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(u,-1));
         visited[u] = true;
-        for(int v : adj.get(u)){
-            if(v==parent) continue;
-            if( visited[v]) return true;
-            
-            if(isCycleDfs(adj,v,visited,u)) return true;
+        
+        while(!q.isEmpty()){
+            Pair p = q.poll();
+            int source = p.node;
+            int parent = p.parent;
+            for(int v : adj.get(source)){
+                if(!visited[v]){
+                    visited[v] = true;
+                    q.add(new Pair(v,source));
+                }
+                else if(v != parent){
+                    return true;
+                }
+            }
         }
         return false;
+    }
+    
+}
+
+
+
+class Pair{
+    int node;
+    int parent;
+    
+    public Pair(int node , int parent){
+        this.node = node;
+        this.parent = parent;
     }
 }
 
